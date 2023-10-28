5858ed74608b84b7648c377b249c93e768b2dd39
mohamedtareq24 <1900889@eng.asu.edu.eg>
2023-10-28 19:38:20 +0200

    register_map edited

--- rtl/dds/register_map.sv
+++ rtl/dds/register_map.sv
@@ -1,44 +1,99 @@
 /*
 This is register map for the DDS core 
 3 x 32 bit Registers are used CTRL , DATA and STAT
+STAT register is saving the current sample of the DDS core
 */
 module register_map (
-    input                                       clk                 ,
-    input                                       a_rst_n             ,
+    input                                       clk                     ,
+    input                                       a_rst_n                 ,
+// Bus 
+    input                                       i_reg_map_write         ,
+    input                                       i_reg_map_read          ,
+    input                 [31:0]                i_reg_map_addrs         ,
+    input   logic signed  [SIG_WIDTH-1:0]       i_reg_map_writedata     ,
+    output  logic         [31:0]                o_reg_map_readdata      ,
 
-    input                 [31:0]                i_reg_map_addrs     ,
-    input   logic signed  [SIG_WIDTH-1:0]       i_reg_map_data      ,
-    input   logic signed  [SIG_WIDTH-1:0]       i_reg_map_signal    ,
-
-    output                                      o_reg_map_rst       ,
-    output                                      o_reg_map_start     ,
-    output  logic signed  [SIG_WIDTH-1:0]       o_reg_map_fifo_data ,
-    
+// core 
+    input   logic signed  [SIG_WIDTH-1:0]       i_reg_map_signal        ,
+    output  logic         [31:0]                o_reg_map_ctrl_reg      ,
+    output  logic         [31:0]                o_reg_map_data_reg      ,
+    output  logic         [31:0]                o_reg_map_stat_reg      ,
+    output  logic         [31:0]                o_reg_map_lngth_reg     ,
+    output  logic         [31:0]                o_reg_map_clk_div_reg   
 );
 
-logic   [31:0]  ctrl_reg    ;
-logic   [31:0]  data_reg    ;
-logic   [31:0]  stat_reg    ;
+parameter   SIG_WIDTH   =   16  ;
+parameter   CTRL        =   0   ; 
+parameter   DATA        =   1   ;  
+parameter   CLKDIV      =   2   ;
+parameter   STAT        =   3   ;
+parameter   LNGTH       =   4   ;
 
+logic   [31:0]  registers [8] ;
+
+// logic   [31:0]  ctrl_reg    ;
+// logic   [31:0]  data_reg    ;
+// logic   [31:0]  stat_reg    ;
+// logic   [31:0]  clk_div_reg ;  
+
+// writable registers
 always @(posedge clk or negedge a_rst_n)       
 begin
     if (!a_rst_n)
     begin
-        ctrl_reg    <=  0   ;
-        data_reg    <=  0   ;
-        stat_reg    <=  0   ;
+        registers [CTRL]    <=  0   ;
+        registers [DATA]    <=  0   ;
+        registers [CLKDIV]  <=  0   ;  
+        registers [LNGTH]   <=  0   ;
     end
-    else
+    else if (i_reg_map_write)
     begin
         case (i_reg_map_addrs)
-            CTRL    :   ctrl_reg    <=  i_reg_map_data      ;
-            DATA    :   data_reg    <=  i_reg_map_data      ;
-            STAT    :   stat_reg    <=  i_reg_map_signal    ;
-            default: 
+            CTRL    :   registers [CTRL]    <=  i_reg_map_writedata      ;
+            DATA    :   registers [DATA]    <=  i_reg_map_writedata      ;
+            CLKDIV  :   registers [CLKDIV]  <=  i_reg_map_writedata      ;
+            LNGTH   :   registers [LNGTH]   <=  i_reg_map_writedata      ;
         endcase
     end
 end
 
+// read only registers
+always @(posedge clk or negedge a_rst_n)       
+begin
+    if (!a_rst_n)
+    begin
+        registers [STAT]    <=  0   ;
+    end
+    else
+    begin
+        registers [STAT]    <=  i_reg_map_signal   ;
+    end
+end
+
+// 
+always @(*)
+begin
+    if (i_reg_map_read)
+    case (i_reg_map_addrs)
+        CTRL    :   o_reg_map_readdata  =   registers[CTRL]     ;
+        DATA    :   o_reg_map_readdata  =   registers[DATA]     ; 
+        CLKDIV  :   o_reg_map_readdata  =   registers[CLKDIV]   ;
+        STAT    :   o_reg_map_readdata  =   registers[STAT]     ;
+        LNGTH   :   o_reg_map_readdata  =   registers[LNGTH]    ;
+        
+        default :   o_reg_map_readdata  =   registers[STAT]     ;
+    endcase
+    else
+        o_reg_map_readdata  =   32'b0   ;
+end
+
+always @(*) begin
+o_reg_map_ctrl_reg      =   registers[CTRL];
+o_reg_map_data_reg      =   registers[DATA];
+o_reg_map_stat_reg      =   registers[STAT];
+o_reg_map_lngth_reg     =   registers[LNGTH];
+o_reg_map_clk_div_reg   =   registers[CLKDIV];
+end
 
 
 
