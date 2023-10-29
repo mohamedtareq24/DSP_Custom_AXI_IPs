341670aa10c07ef2be891f96606b54cac3d0e472
mohamedtareq24 <1900889@eng.asu.edu.eg>
2023-10-29 03:05:00 +0200

    dds+register_map

--- rtl/dds/dds.sv
+++ rtl/dds/dds.sv
@@ -1,21 +1,20 @@
 module dds ( 
-    input                                       clk             ,
-    input                                       a_rst_n         ,
+    input                                       clk                 ,
+    input                                       a_rst_n             ,
     // bus intreface
-    input                   [31:0]              i_dds_addrs     ,           //THETAS,AMPLS or DELTAS 
-    input   logic signed    [SIG_WIDTH-1:0]     i_dds_fifo_data ,       
-
+    input                   [31:0]              i_dds_addrs         ,      //THETAS,AMPLS or DELTAS
+    input                                       i_dds_write         ,              
     //register map
-    input                   [31:0]              i_dds_ctrl_reg  ,
-    input                   [31:0]              i_dds_data_reg  ,
-    input                   [31:0]              i_dds_stat_reg  ,
-
+    input                   [31:0]              i_dds_ctrl_reg      ,
+    input                   [31:0]              i_dds_thetas_reg    ,
+    input                   [31:0]              i_dds_deltas_reg    ,
+    input                   [31:0]              i_dds_ampls_reg     ,
+    input                   [31:0]              i_dds_lngth_reg     ,
+    input                   [31:0]              i_dds_clk_div_reg   ,
     //sample timer 
-    input                                       i_dds_sample_en ,           // updates the sample 
-
-
-    // all registers are read,write  
-    output  logic signed  [SIG_WIDTH-1:0]       o_dds_signal                // DDS output 
+    input                                       i_dds_sample_en    ,        // updates the sample 
+    // DDS output 
+    output  logic signed    [SIG_WIDTH-1:0]     o_dds_signal       
 );
 
 parameter 	SIG_WIDTH		=	16;
@@ -24,8 +23,8 @@ parameter   THETAS          =    0;
 parameter   DELTAS          =    1;
 parameter   AMPLS           =    2;
 
-parameter   DDS_RST_BIT     =   0;
-parameter   DDS_STRT_BIT    =   1;
+parameter   DDS_RST_BIT     =    0;
+parameter   DDS_STRT_BIT    =    1;
 
 logic                                   thetas_en ,     deltas_en   ,   ampls_en        ;
 logic   signed      [SIG_WIDTH-1:0]     thetas_in ,     deltas_in   ,   ampls_in        ; 
@@ -34,14 +33,92 @@ logic   signed      [SIG_WIDTH-1:0]     theta_reg ,     deltas_reg  ,   ampls_re
 logic   signed      [SIG_WIDTH-1:0]     sin_index ,     sin_out     ,   sin_index_temp  ;
 logic   signed      [SIG_WIDTH-1:0]     mult_out  ,     accmltor    ;
 
-logic                                       dds_rst       ,           // soft reset, reset before writing any data 
-logic                                       dds_start     ,
+logic                   dds_rst       ;           // soft reset, reset before writing any data 
+logic                   dds_start     ;
 
-assign  dds_rst         =   ctrl_reg [DDS_RST_BIT]  ;
-assign  dds_start       =   ctrl_reg [DDS_STRT_BIT] ;
+logic   [SIG_WIDTH-1:0] deltas_feedback_tap ;
+logic   [SIG_WIDTH-1:0] ampls_feedback_tap  ;
+logic   [SIG_WIDTH-1:0] thetas_feedback_tap ;
 
+logic   [SIG_WIDTH-1:0] thetas_tap1         ;
+logic   [SIG_WIDTH-1:0] thetas_tap8         ;
+logic   [SIG_WIDTH-1:0] thetas_tap16        ;
+logic   [SIG_WIDTH-1:0] thetas_tap32        ;
+logic   [SIG_WIDTH-1:0] thetas_tap64        ;
+logic   [SIG_WIDTH-1:0] thetas_tap128       ;
+logic   [SIG_WIDTH-1:0] thetas_tap256       ;
+logic   [SIG_WIDTH-1:0] thetas_tap512       ;
 
-always_comb begin : fifo_addrs_decoder
+logic   [SIG_WIDTH-1:0] deltas_tap1         ;
+logic   [SIG_WIDTH-1:0] deltas_tap8         ;
+logic   [SIG_WIDTH-1:0] deltas_tap16        ;
+logic   [SIG_WIDTH-1:0] deltas_tap32        ;
+logic   [SIG_WIDTH-1:0] deltas_tap64        ;
+logic   [SIG_WIDTH-1:0] deltas_tap128       ;
+logic   [SIG_WIDTH-1:0] deltas_tap256       ;
+logic   [SIG_WIDTH-1:0] deltas_tap512       ;
+
+logic   [SIG_WIDTH-1:0] ampls_tap1          ;
+logic   [SIG_WIDTH-1:0] ampls_tap8          ;
+logic   [SIG_WIDTH-1:0] ampls_tap16         ;
+logic   [SIG_WIDTH-1:0] ampls_tap32         ;
+logic   [SIG_WIDTH-1:0] ampls_tap64         ;
+logic   [SIG_WIDTH-1:0] ampls_tap128        ;
+logic   [SIG_WIDTH-1:0] ampls_tap256        ;
+logic   [SIG_WIDTH-1:0] ampls_tap512        ;
+
+assign  dds_rst         =   i_dds_ctrl_reg [DDS_RST_BIT]  ;
+assign  dds_start       =   i_dds_ctrl_reg [DDS_STRT_BIT] ;
+
+//// tap mux 
+always @ (*) 
+begin
+    case (i_dds_lngth_reg)
+        1 : begin 
+            deltas_feedback_tap     =   deltas_tap1 ;
+            ampls_feedback_tap      =   ampls_tap1  ;
+            thetas_feedback_tap     =   thetas_tap1 ;
+        end
+        8 : begin
+            deltas_feedback_tap     =   deltas_tap8 ;
+            ampls_feedback_tap      =   ampls_tap8  ;
+            thetas_feedback_tap     =   thetas_tap8 ;
+        end
+        16:begin
+            deltas_feedback_tap     =   deltas_tap16 ;
+            ampls_feedback_tap      =   ampls_tap16  ;
+            thetas_feedback_tap     =   thetas_tap16 ;
+        end
+        32:begin
+            deltas_feedback_tap     =   deltas_tap32 ;
+            ampls_feedback_tap      =   ampls_tap32  ;
+            thetas_feedback_tap     =   thetas_tap32 ;
+        end
+        64:begin
+            deltas_feedback_tap     =   deltas_tap64    ;
+            ampls_feedback_tap      =   ampls_tap64     ;
+            thetas_feedback_tap     =   thetas_tap64    ;
+        end
+        128:begin
+            deltas_feedback_tap     =   deltas_tap128   ;
+            ampls_feedback_tap      =   ampls_tap128    ;
+            thetas_feedback_tap     =   thetas_tap128   ;
+        end
+        256:begin
+            deltas_feedback_tap     =   deltas_tap256   ;
+            ampls_feedback_tap      =   ampls_tap256    ;
+            thetas_feedback_tap     =   thetas_tap256   ;
+        end
+        default:begin
+            deltas_feedback_tap     =   deltas_out  ;
+            ampls_feedback_tap      =   ampls_out   ; 
+            thetas_feedback_tap     =   thetas_out  ;
+        end 
+    endcase
+end
+
+always_comb 
+begin 
     thetas_en   =   0;
     thetas_in   =   0;
     deltas_en   =   0;
@@ -49,52 +126,69 @@ always_comb begin : fifo_addrs_decoder
     ampls_en    =   0;
     ampls_in    =   0;
     
-    if (i_dds_start)    /// buffer circulats the data 
-    begin
+    if (dds_start)                      /// buffer circulats the data 
+    begin                               /// taps to be added here
         thetas_en   =   1;
         deltas_en   =   1;
         ampls_en    =   1;
-        thetas_in   =   sin_index_temp  ;
-        deltas_in   =   deltas_out      ;
-        ampls_in    =   ampls_out       ;
+        thetas_in   =   thetas_feedback_tap         ;
+        deltas_in   =   deltas_feedback_tap         ;
+        ampls_in    =   ampls_feedback_tap          ;
     end
     else
     begin
-        case (i_dds_addrs)
-            THETAS  :  
-            begin
-                thetas_en   =   1;
-                thetas_in   =   i_dds_fifo_data;
-            end 
-            DELTAS  :  
-            begin
-                deltas_en   =   1;
-                deltas_in   =   i_dds_fifo_data;
-            end
-            AMPLS  :  
-            begin
-                ampls_en   =   1;
-                ampls_in   =   i_dds_fifo_data;
-            end  
-        endcase
+        if (i_dds_write)
+        begin
+            case (i_dds_addrs)
+                THETAS  :  
+                begin
+                    thetas_en   =   1;
+                    thetas_in   =   i_dds_thetas_reg;
+                end 
+                DELTAS  :  
+                begin
+                    deltas_en   =   1;
+                    deltas_in   =   i_dds_deltas_reg;
+                end
+                AMPLS  :  
+                begin
+                    ampls_en   =   1;
+                    ampls_in   =   i_dds_ampls_reg;
+                end  
+            endcase
+        end
     end
 end
 
 
 shift_reg thetas_fifo (
     .clk    (clk)           ,
-    .rst    (dds_rst)     ,
+    .rst    (dds_rst)       ,
     .en     (thetas_en)     ,
     .sr_in  (thetas_in)     ,
-    .sr_out (thetas_out)    
+    .sr_1   (thetas_tap1)   ,
+    .sr_8   (thetas_tap8)   ,
+    .sr_16  (thetas_tap16)  ,
+    .sr_32  (thetas_tap32)  ,
+    .sr_64  (thetas_tap64)  ,
+    .sr_128 (thetas_tap128) ,
+    .sr_256 (thetas_tap256) ,
+    .sr_out (thetas_tap512)    
 );
 
 shift_reg deltas_fifo (
     .clk    (clk)           ,
-    .rst    (dds_rst)     ,
+    .rst    (dds_rst)       ,
     .en     (deltas_en)     ,
     .sr_in  (deltas_in)     ,
-    .sr_out (deltas_out)
+    .sr_1   (deltas_tap1)   ,
+    .sr_8   (deltas_tap8)   ,
+    .sr_16  (deltas_tap16)  ,
+    .sr_32  (deltas_tap32)  ,
+    .sr_64  (deltas_tap64)  ,
+    .sr_128 (deltas_tap128) ,
+    .sr_256 (deltas_tap256) ,
+    .sr_out (deltas_tap512)
 );
 
 always_ff @( posedge clk or negedge a_rst_n ) 
@@ -115,10 +209,10 @@ begin
     end
     else
     begin
-        theta_reg       <=  thetas_out  ;
-        deltas_reg      <=  deltas_out  ;      
-        ampls_reg_dly   <=  ampls_reg   ;
-        ampls_reg       <=  ampls_out   ;    
+        theta_reg       <=  thetas_feedback_tap  ;
+        deltas_reg      <=  deltas_feedback_tap  ;      
+        ampls_reg_dly   <=  ampls_reg            ;
+        ampls_reg       <=  ampls_feedback_tap   ;    
     end
 end
 
@@ -134,10 +228,17 @@ sin_lut  lut
 
 shift_reg ampls_fifo (
     .clk    (clk)           ,
-    .rst    (dds_rst)     ,
+    .rst    (dds_rst)       ,
     .en     (ampls_en)      ,
     .sr_in  (ampls_in)      ,
-    .sr_out (ampls_out)
+    .sr_1   (ampls_tap1)    ,
+    .sr_8   (ampls_tap8)    ,
+    .sr_16  (ampls_tap16)   ,
+    .sr_32  (ampls_tap32)   ,
+    .sr_64  (ampls_tap64)   ,
+    .sr_128 (ampls_tap128)  ,
+    .sr_256 (ampls_tap256)  ,
+    .sr_out (ampls_tap512)
 );
 
 assign  mult_out = $signed(ampls_reg_dly) * $signed(sin_out) ;
--- rtl/dds/register_map.sv
+++ rtl/dds/register_map.sv
@@ -1,6 +1,5 @@
 /*
 This is register map for the DDS core 
-3 x 32 bit Registers are used CTRL , DATA and STAT
 STAT register is saving the current sample of the DDS core
 */
 module register_map (
@@ -16,25 +15,26 @@ module register_map (
 // core 
     input   logic signed  [SIG_WIDTH-1:0]       i_reg_map_signal        ,
     output  logic         [31:0]                o_reg_map_ctrl_reg      ,
-    output  logic         [31:0]                o_reg_map_data_reg      ,
+    output  logic         [31:0]                o_reg_map_thetas_reg    ,
+    output  logic         [31:0]                o_reg_map_delats_reg    ,
+    output  logic         [31:0]                o_reg_map_ampls_reg     ,
     output  logic         [31:0]                o_reg_map_stat_reg      ,
     output  logic         [31:0]                o_reg_map_lngth_reg     ,
     output  logic         [31:0]                o_reg_map_clk_div_reg   
 );
 
 parameter   SIG_WIDTH   =   16  ;
+
+// registers
 parameter   CTRL        =   0   ; 
-parameter   DATA        =   1   ;  
-parameter   CLKDIV      =   2   ;
-parameter   STAT        =   3   ;
-parameter   LNGTH       =   4   ;
+parameter   THETAS      =   1   ;
+parameter   DELTAS      =   2   ;
+parameter   AMPLS       =   3   ;
+parameter   CLKDIV      =   4   ;
+parameter   STAT        =   5   ;
+parameter   LNGTH       =   6   ;
 
 logic   [31:0]  registers [8] ;
-
-// logic   [31:0]  ctrl_reg    ;
-// logic   [31:0]  data_reg    ;
-// logic   [31:0]  stat_reg    ;
-// logic   [31:0]  clk_div_reg ;  
 
 // writable registers
 always @(posedge clk or negedge a_rst_n)       
@@ -42,17 +42,21 @@ begin
     if (!a_rst_n)
     begin
         registers [CTRL]    <=  0   ;
-        registers [DATA]    <=  0   ;
         registers [CLKDIV]  <=  0   ;  
         registers [LNGTH]   <=  0   ;
+        registers [THETAS]  <=  0   ;
+        registers [DELTAS]  <=  0   ;
+        registers [AMPLS]   <=  0   ;
     end
     else if (i_reg_map_write)
     begin
         case (i_reg_map_addrs)
-            CTRL    :   registers [CTRL]    <=  i_reg_map_writedata      ;
-            DATA    :   registers [DATA]    <=  i_reg_map_writedata      ;
-            CLKDIV  :   registers [CLKDIV]  <=  i_reg_map_writedata      ;
-            LNGTH   :   registers [LNGTH]   <=  i_reg_map_writedata      ;
+            CTRL    :   registers [CTRL]    <=  i_reg_map_writedata         ;
+            CLKDIV  :   registers [CLKDIV]  <=  i_reg_map_writedata         ;
+            LNGTH   :   registers [LNGTH]   <=  i_reg_map_writedata         ;
+            THETAS  :   registers [THETAS]  <=  i_reg_map_writedata         ;
+            DELTAS  :   registers [DELTAS]  <=  i_reg_map_writedata         ;
+            AMPLS   :   registers [AMPLS]   <=  i_reg_map_writedata         ;
         endcase
     end
 end
@@ -76,11 +80,13 @@ begin
     if (i_reg_map_read)
     case (i_reg_map_addrs)
         CTRL    :   o_reg_map_readdata  =   registers[CTRL]     ;
-        DATA    :   o_reg_map_readdata  =   registers[DATA]     ; 
+        THETAS  :   o_reg_map_readdata  =   registers[THETAS]   ;
+        DELTAS  :   o_reg_map_readdata  =   registers[DELTAS]   ;
+        AMPLS   :   o_reg_map_readdata  =   registers[AMPLS]    ;
         CLKDIV  :   o_reg_map_readdata  =   registers[CLKDIV]   ;
         STAT    :   o_reg_map_readdata  =   registers[STAT]     ;
         LNGTH   :   o_reg_map_readdata  =   registers[LNGTH]    ;
-        
+
         default :   o_reg_map_readdata  =   registers[STAT]     ;
     endcase
     else
@@ -88,11 +94,13 @@ begin
 end
 
 always @(*) begin
-o_reg_map_ctrl_reg      =   registers[CTRL];
-o_reg_map_data_reg      =   registers[DATA];
-o_reg_map_stat_reg      =   registers[STAT];
-o_reg_map_lngth_reg     =   registers[LNGTH];
-o_reg_map_clk_div_reg   =   registers[CLKDIV];
+o_reg_map_ctrl_reg      =   registers[CTRL]     ;
+o_reg_map_thetas_reg    =   registers[THETAS]   ;
+o_reg_map_delats_reg    =   registers[DELTAS]   ;
+o_reg_map_ampls_reg     =   registers[AMPLS]    ;
+o_reg_map_stat_reg      =   registers[STAT]     ;
+o_reg_map_lngth_reg     =   registers[LNGTH]    ;
+o_reg_map_clk_div_reg   =   registers[CLKDIV]   ;
 end
 
 
